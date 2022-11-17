c510fa4e0d0ebb3187bfc80ab3d43b1222451e5c
camilo ardila <miloardila98@gmail.com>
2022-11-17 02:57:03 -0500

    primeros pasos y query seletors

---
+++ js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const inputEmail = document.querySelector('#email');
+  const inputAsunto = document.querySelector('#asunto');
+  const inputMensaje = document.querySelector('#mensaje');
+
+
+  console.log(inputMensaje);
+
+})
