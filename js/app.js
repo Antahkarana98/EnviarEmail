24d596b6d8c84556798d431e25debac003269e8c
camilo ardila <miloardila98@gmail.com>
2022-12-13 21:56:09 -0500

    ver si los formularios estan vacios y generar un pequeño HTML

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  //Seleccionando los elementos de la interfaz
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
 
+  //agregando eventos a estos elementos
 
-  console.log(inputMensaje);
+  inputEmail.addEventListener('blur', validacion);
+  inputAsunto.addEventListener('blur', validacion);
+  inputMensaje.addEventListener('blur', validacion);
 
+
+  //funcion para validar los campos y optimizar el codigo
+  function validacion(e) {
+
+    //como saber si el campo ingresado esta vacio, trim es para eliminar espacios en blanco
+    if(e.target.value.trim() === '') {
+      alertaError();
+    } else{
+      console.log("Tiene algo...");
+    }
+  }
+
+  function alertaError() {
+    const error = document.createElement('P');
+
+    error.textContent = 'Hubo un error...';
+    console.log(error);
+    console.log(error);
+  }
 })
